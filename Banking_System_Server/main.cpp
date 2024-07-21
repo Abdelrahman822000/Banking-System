ece44d8839a4d124c1caa475eef108b77c750798
Abdelrahman822000 <a.mohamed822000@gmai.com>
2024-07-21 17:25:12 +0300

    Final Version

--- Banking_System_Server/main.cpp
+++ Banking_System_Server/main.cpp
@@ -1,11 +1,17 @@
-#include <QCoreApplication>
-#include <Server.h>
+#include <QCoreApplication>  // Include the QCoreApplication class for creating a console application
+#include <Server.h>          // Include the custom Server class header
+
 int main(int argc, char *argv[])
 {
+    // Create a QCoreApplication object, which manages application-wide resources and settings
     QCoreApplication a(argc, argv);
 
+    // Create an instance of the Server class
     Server server;
+
+    // Start the server
     server.startServer();
 
+    // Enter the event loop to wait for and dispatch events
     return a.exec();
 }
